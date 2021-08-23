4a5a3155947c1324d1b82a1b57414af2d632570c
UmarH619 <umarhussain1997@gmail.com>
2021-08-23 13:29:14 +0100

    added arrays of data

---
+++ Script.js
@@ -0,0 +1,5 @@
+let destination = ["England", "Finland", "France", "Spain", "Switzerland", "USA", "Hawaii", "New Zealand", "Australia", "Antarctica", "China", "Japan", "Mars"];
+let transport = ["walking", "driving", "plane", "boat", "rocket", "swimming", "skating"];
+let activity = ["sightseeing", "skiing", "iceskating", "shopping", "jet skiing", "bungee jumping", "escape rooms"];
+
+
